109f78d45944a58db91586a7dec87a86e2564fd3
axeliono <69375446+axeliono@users.noreply.github.com>
2020-12-07 19:06:20 -0600

    MVP complete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require("./data/animals.json");
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const filterByQuery = (query, animalsArray) => {
@@ -46,6 +47,12 @@ const filterByQuery = (query, animalsArray) => {
   return filteredResults;
 };
 
+const findById = (id, animalsArray) => {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+};
+
+// GET routes
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -54,6 +61,15 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
